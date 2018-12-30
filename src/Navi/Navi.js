7d54271f3ead14881da58516c91ccc3aeb77b413
hugo tang <hugotang@192.168.1.101>
2018-12-30 12:26:47 +0800

    初始化项目

---
+++ src/Navi/Navi.js
@@ -0,0 +1,98 @@
+import { Layout, Menu, Breadcrumb, Icon ,Button} from 'antd';
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import logo from '../logo.svg';
+import './Navi.css'
+const { Header, Content, Footer, Sider } = Layout;
+const SubMenu = Menu.SubMenu
+
+class SiderDemo extends Component {
+    state = {
+        collapsed: false,
+        mode: 'inline',
+    };
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    }
+
+    render() {
+        return (
+            <Layout>
+                <Sider
+                    trigger={null}
+                    collapsible
+                    collapsed={this.state.collapsed}
+                >
+                    <div className="logo" />
+                    <Menu
+                    defaultSelectedKeys={['1']}
+                    defaultOpenKeys={['sub1']}
+                    mode="inline"
+                    theme="dark"
+                    inlineCollapsed={this.state.collapsed}
+                    >
+                    <Menu.Item key="1">
+                        <Icon type="home" />
+                        <span>Home</span>
+                    </Menu.Item>
+                    <Menu.Item key="2">
+                        <Icon type="mobile" />
+                        <span>MyApp</span>
+                    </Menu.Item>
+                    <Menu.Item key="3">
+                        <Icon type="team" />
+                        <span>Users</span>
+                    </Menu.Item>
+                    <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+                        <Menu.Item key="5">Option 5</Menu.Item>
+                        <Menu.Item key="6">Option 6</Menu.Item>
+                        <Menu.Item key="7">Option 7</Menu.Item>
+                        <Menu.Item key="8">Option 8</Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
+                        <Menu.Item key="9">Option 9</Menu.Item>
+                        <Menu.Item key="10">Option 10</Menu.Item>
+                        <SubMenu key="sub3" title="Submenu">
+                        <Menu.Item key="11">Option 11</Menu.Item>
+                        <Menu.Item key="12">Option 12</Menu.Item>
+                        </SubMenu>
+                    </SubMenu>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header style={{ background: '#000', padding: 0 }}>
+                        <span style={{color:'#fff', paddingLeft:'2%', fontSize:'1.4em'}}>
+                            <Icon
+                                className="trigger"
+                                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                                onClick={this.toggle}
+                                style={{cursor: 'pointer'}}
+                            />
+                        </span>
+                        <span style={{color:'#fff', paddingLeft:'2%', fontSize:'1.4em'}}>Information Management System</span>
+                        <span style={{color:'#fff', float:'right', paddingRight:'1%'}}>
+                            <img src={logo} className="App-logo" alt="logo" />
+                        </span>
+                    </Header>
+                    <Content style={{ margin: '0 16px' }}>
+                        <Breadcrumb style={{ margin: '12px 0' }}>
+                            <Breadcrumb.Item>User</Breadcrumb.Item>
+                            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <div style={{ padding: 24, background: '#fff', minHeight: 780 }}>
+
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>
+                        Ant Design ©2016 Created by Ant UED
+                    </Footer>
+                </Layout>
+            </Layout>
+        );
+    }
+}
+
+export default SiderDemo;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -10,3 +10,76 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+import { Layout, Menu, Icon } from 'antd';
+import React,{Component} from 'react'
+import ReactDOM from 'react-dom'
+import './index.css';
+
+const { Header, Sider, Content } = Layout;
+
+class SiderDemo extends Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span>nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span>nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span>nav 3</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className="trigger"
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+          </Header>
+          <Content style={{
+            margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280,
+          }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    );
+  }
+}
+
+ReactDOM.render(<SiderDemo />, document.getElementById("root"));
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SiderDemo from './Navi/Navi'
+//import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+ReactDOM.render(<SiderDemo />, document.getElementById('root'));
+//registerServiceWorker();
